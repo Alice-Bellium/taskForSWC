bafdf1b972439344e646a68fac28ce7ec288d553
alisa.battalova <alisa.battalova@tatar.ru>
2023-10-20 17:30:44 +0600

    T2 | Миграции и модели
    Создание таблиц events и event_user, добавление колонок last_name, date_of_birth к таблице user, изменение названия колонки name на first_name.
    Создание модели Event, доработка модели User

---
+++ app/Models/Event.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * @property int $id Идентификатор записи
+ * @property string $title Заголовок
+ * @property string $text Текст
+ * @property int $creator_user_id Идентификатор пользователя
+ * @property-read Carbon $created_at Дата создания
+ * @property-read Carbon $updated_at Дата обновления
+ */
+class Event extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'text',
+        'creator_user_id',
+    ];
+
+    /**
+     * The event creator user.
+     */
+    public function creatorUser(): BelongsTo {
+        return $this->belongsTo(User::class, 'creator_user_id');
+    }
+
+    /**
+     * The users that belong to the event.
+     */
+        public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,28 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
+/**
+ * @property int $id Идентификатор записи
+ * @property string $first_name Имя
+ * @property string $last_name Фамилия
+ * @property string $password Пароль
+ * @property string $email Адрес электронной почты
+ * @property Carbon $date_of_birth Дата рождения
+ * @property-read Carbon $created_at Дата создания
+ * @property-read Carbon $updated_at Дата обновления
+ * @property-read Carbon $remember_token Токен
+ * @property-read Carbon $email_verified_at Дата подтверждения электронной почты
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -18,9 +35,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'first_name',
+        'last_name',
         'password',
+        'email',
+        'date_of_birth',
     ];
 
     /**
@@ -41,5 +60,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'date_of_birth' => 'datetime',
     ];
+
+    /**
+     * The events that were created by the user.
+     */
+    public function createdEvents(): HasMany {
+        return $this->hasMany(Event::class, 'creator_user_id');
+    }
+
+    /**
+     * The events that belong to the user.
+     */
+    public function events(): BelongsToMany
+    {
+        return $this->belongsToMany(Event::class);
+    }
 }
